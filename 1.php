fc919beda09a902537dd723d410a0f06a05d342e
李增光 <lizengguangxx@126.com>
2017-02-16 00:02:46 +0800

    李增光
    https://github.com/lizengguangxx/2.15

---
+++ 1.php
@@ -0,0 +1,38 @@
+<?php
+	header("Content-Type:text/html;charset=UTF-8");
+	
+//	if($_GET["name"]=="zhangsan"){
+//		echo "<h1>ok</h1>";
+//	}else{
+//		echo "error";
+//	};
+//	echo $_POST["name"];
+
+	echo "<h1>ok</h1>";
+
+
+
+//	$aa=array(
+//		0=>"a"
+//	)
+//	echo $aa=[0];
+//	
+//	function aa(){
+//	
+//	}
+//	aa();
+//	
+//	$str="名字";
+//	$str1="111";
+//	echo $str.$str1;
+	/*
+	 * 关联数组
+	 * 索引数组
+	 * 
+	 * 分号和回车  他是解耦的，只有分号代表一行命令的结束
+	 * 
+	 * 
+	 */
+	
+	
+?>
