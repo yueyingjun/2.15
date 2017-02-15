2069e1082eba2ac8d6df72f8ebf094818fc152c8
gys <245882185@qq.com>
2017-02-15 22:33:37 +0800

    mm

---
+++ lx.php
@@ -0,0 +1,17 @@
+<?php
+//	GET
+//$name=$_GET["name"];
+//if($name=="gys"){
+//	echo "gys";
+//}else{
+//	echo "请求失败";
+//}
+//post
+$name=$_POST["name"];
+//if($name=="gys"){
+//	echo "gys";
+//}else{
+//	echo "请求失败";
+//}
+echo $name;
+?>
